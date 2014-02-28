ddc6b639911ece58f88dde6e64ada4ca3f53e676
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-02-28 23:26:33 +0100

    Bash gui works with sequences of bars.

--- gui-bash.js
+++ gui-bash.js
@@ -1,30 +1,21 @@
 #!/usr/bin/env node
 
 // ^ . . .
-var m = require('src/metronome');
-var Metronome = m.Metronome;
-var Bar = m.Bar;
+var Metronome = require('src/metronome');
+var Sequence = require('src/sequence');
+var Bar = require('src/bar');
 
 var m = new Metronome({
-  bar: new Bar(11, 8),
-  bpm: 220
+  seq: new Sequence(220, [ new Bar(11, 8), new Bar(4, 4) ])
 });
 m.start();
-
-var wasH = false;
 
 m.on('beat', function (level) {
   if (level == 'h') {
     process.stdout.clearLine();
     process.stdout.write('\r#');
-    wasH = true;
   }
   else {
-    if (wasH) {
-      wasH = false;
-      process.stdout.clearLine();
-      process.stdout.write('\r  ');
-    }
     process.stdout.write('.');
   }
   process.stdout.write(' ');
